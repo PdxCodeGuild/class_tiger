93123e596cf3dab153c59a32078b3468f0f3ac46
Tate <tateshep@gmail.com>
2019-04-09 16:25:56 -0700

    added arithmetic eval to lab11

--- code/tate/Labs/lab11.py
+++ code/tate/Labs/lab11.py
@@ -4,20 +4,25 @@ calc_result = 0
 
 while True:
     print('Type \'done\' to quit.')
-    user_operator = input('What is the operation you would like to perform? \n (+, -, *, or /) : ')
+    user_operator = input('What is the operation you would like to perform? \n (+, -, *, /, or arithmetic) : ')
 
     if user_operator.lower() == 'done':
         print('Goodbye!')
         break
 
-    while user_operator not in ['+','-','*','/','done']:
+    while user_operator not in ['+','-','*','/','done','arithmetic']:
         print('Invalid operator input')
-        user_operator = input('What is the operation you would like to perform? \n (+, -, *, /, or \'done\') : ')
+        user_operator = input('What is the operation you would like to perform? \n (+, -, *, /, arithmetic or \'done\') : ')
 
     # Would change this to not need the following if statement when we cover functions
     if user_operator.lower() == 'done':
         print('Goodbye!')
         break
+
+    if user_operator == 'arithmetic':
+        calc_result = eval(input('what would you like to evaluate? : '))
+        print(f'Your result is {calc_result}')
+        continue
 
     operand1 = int(input('What is the first number? : '))
     operand2 = int(input('What is the second number? : '))
@@ -31,4 +36,4 @@ while True:
     elif user_operator == '/':
         calc_result = operand1 / operand2
 
-    print(calc_result)
+    print(f'Your result is {calc_result}')
