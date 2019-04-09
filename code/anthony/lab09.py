141e828d7113d3bc19daf2b46d2b4c335f39d065
Anthony W <anthonyt.wallace@icloud.com>
2019-04-09 11:42:26 -0700

    Lab09 added protection for unwatned input

--- code/anthony/lab09.py
+++ code/anthony/lab09.py
@@ -5,18 +5,27 @@ base_dict = {'binary': 2, 'decimal': 10, 'hexadecimal': 16}
 
 in_num = float(input("How many?: "))
 in_unit = input("From what unit? [m, ft, in, yd, km, mi]: ")
+while in_unit not in meter_dict:
+    in_unit = input("From what unit? [m, ft, in, yd, km, mi]: ")
+
 out_unit = input("To what unit? [m, ft, in, yd, km, mi]: ")
-which_base = base_dict[input("What base would you like to convert to? [binary, decimal, hexadecimal]: ")]
+while out_unit not in meter_dict:
+    out_unit = input("To what unit? [m, ft, in, yd, km, mi]: ")
+
+which_base = input("What base would you like to convert to? [binary, decimal, hexadecimal]: ")
+while which_base not in base_dict:
+    which_base = input("What base would you like to convert to? [binary, decimal, hexadecimal]: ")
+
 converted_num = 0
 # print(f"{in_num}{in_unit} is {(in_num / meter_dict[in_unit])*meter_dict[out_unit]} {out_unit}.")
 decimal_num = (in_num / meter_dict[in_unit])*meter_dict[out_unit]
 
-if which_base != 10:
-    if which_base == 2:
+if base_dict[which_base] != 10:
+    if base_dict[which_base] == 2:
         converted_num = bin(int(decimal_num))
-    elif which_base == 16:
+    elif base_dict[which_base] == 16:
         converted_num = hex(int(decimal_num))
 else: 
     converted_num = decimal_num
 
-print(f"{in_num}{in_unit} is {decimal_num}{out_unit} and is converted to {converted_num}")
+print(f"{in_num}{in_unit} is {decimal_num}{out_unit} and is converted to {converted_num} in {which_base}")
