7b128f7a787a25e0a3477d32f3275ec6b1ff180f
Tabatha Y <tabathadelane@gmail.com>
2019-04-09 16:15:57 -0700

    lab11.3

--- code/Tabatha/Lab11_ver3.py
+++ code/Tabatha/Lab11_ver3.py
@@ -1,5 +1,12 @@
 
-expression = input('I am a Calculator. \n Enter a simple expression > ' )
+expression = input('I am a Calculator. \n Enter a simple expression or type "done" > ' )
 
-total = eval(expression)
-print(f'The answer is {total}')
+while True:
+
+    if expression.lower() == 'done':
+        print("DONE")
+        break
+
+    else:
+        total = eval(expression)
+        print(f'The answer is {total}')
