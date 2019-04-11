122d8d4a12af46e2563f9ce3973b442239d69287
Tate <tateshep@gmail.com>
2019-04-11 14:41:46 -0700

    modified lab19, p02, and p03

--- code/tate/Labs/practice02-strings.py
+++ code/tate/Labs/practice02-strings.py
@@ -30,8 +30,16 @@ def missing_char(user_string):
 
 # print(missing_char(user_string))
 
-# Problem 3 : write a function that returns the number of occurances of 'hi' in a given string_list
+# Problem 3 :
 
+def latest_letter(user_string):
+    user_list = list(user_string.lower())
+    user_list.sort()
+    return user_list[-1]
+
+# print(latest_letter('Whatever'))
+
+# Problem 4 :  write a function that returns the number of occurances of 'hi' in a given string_list
 def count_hi(user_string):
     list_string = list(user_string)
     length_string = len(list_string)
@@ -49,11 +57,42 @@ def count_hi(user_string):
 
     return count_occurances
 
-#
 # count_occurances = count_hi('hi hi hi hi hi')
 # print(count_occurances)
 
+# Problem 5 : Write a function that returns True if a string contains the same number of 'cat ' as it does 'dog'
 
+def cat_dog(user_string):
+    user_string = user_string.lower()
+    cat_count = user_string.count('cat')
+    dog_count = user_string.count('dog')
+
+    if cat_count == dog_count:
+        return True
+    else:
+        return False
+
+# print(cat_dog('catdog dog dog'))
+
+# Problem 6 : Return the number of letter occurances in a string
+
+def count_letter(letter,word):
+    list_word = list(word)
+    list_word.sort()
+    word = ''.join(list_word)
+    letter_count = word.count(letter)
+    return letter_count
+
+# print(count_letter('h','adflsjadhhhskfadsfasd'))
+
+
+# Problem 7 : Convert input strings to lowercase without any surrounding whitespace
+
+def lower_case(input_str):
+    my_string = input_str.strip(' ')
+    return my_string.lower()
+
+# print(lower_case('    Superd NNANNAN Batman     '))
 
 
 
