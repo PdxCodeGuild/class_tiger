b12851b17f47e31a18eda9a977d29a4fb73d8105
Anthony W <anthonyt.wallace@icloud.com>
2019-04-12 16:26:48 -0700

    Updated lab13

--- code/anthony/lab13.py
+++ code/anthony/lab13.py
@@ -1,16 +1,21 @@
 import string
 def main():
     user_string = input("Enter your string to be cyphered: ")
-    cypher = int(input("Enter the amount of encryption: "))
+    while True:
+        try:
+            cypher = int(input("Enter the amount of encryption: "))
+            break
+        except:
+            print("Try again")
+            
     
     encrypted_string = encrypt_string(cypher, user_string)
     print(str(encrypted_string))
 def encrypt_string(num, str):
     encrypted = []
     for char in str:
-        if (ord(char) + num > 122):
-            encrypted.append(chr(ord(char) + (num - 26)))
-        else:
-            encrypted.append(chr(ord(char) + num))
+        while (ord(char)) + num > 122:
+            num = num - 26
+        encrypted.append(chr(ord(char) + num))
     return encrypted
 main()
