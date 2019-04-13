c3b4ba434b283cf8ed6aaa29cad6a2a0ada31494
Tate <tateshep@gmail.com>
2019-04-13 11:11:45 -0700

    updated lab14

--- code/tate/Labs/lab14.py
+++ code/tate/Labs/lab14.py
@@ -4,15 +4,28 @@ Lab 14: Pick6
 import random
 
 def gen_6():
-    return [random.randint(1,30) for n in range(6)]
+    return [random.randint(1,99) for n in range(6)]
 
 winning_ticket = gen_6()
+
 
 ######
 def play_game():
     balance = 0
-    for n in range(10000):
-        balance -= 2
+    ticket_cost = 2
+    times_played = 100000
+    earnings = 0
+    match1 = 0
+    match2 = 0
+    match3 = 0
+    match4 = 0
+    match5 = 0
+    match6 = 0
+
+    for n in range(times_played):
+
+
+        balance -= ticket_cost
         my_ticket = gen_6()
         count_matches = 0
 
@@ -22,23 +35,34 @@ def play_game():
 
         if count_matches == 1:
             balance += 4
-            print(balance, ' 4')
+            earnings += 4
+            match1 += 1
         elif count_matches == 2:
             balance += 7
-            print(balance, ' 7')
+            earnings += 7
+            match2 += 1
         elif count_matches == 3:
             balance += 100
-            print(balance, ' 100')
+            earnings += 100
+            match3 += 1
         elif count_matches == 4:
             balance += 50000
-            print(balance, ' 50000')
+            earnings += 50000
+            match4 += 1
         elif count_matches == 5:
             balance += 1000000
-            print(balance, ' 100000')
+            earnings += 1000000
+            match5 += 1
         elif count_matches == 6 :
             balance += 25000000
-            print(balance, ' 25000000')
+            earnings += 25000000
+            match6 += 1
 
-    return balance
+        expenses= ticket_cost * times_played
+        roi = (earnings - expenses) / expenses
+
+    # return f'Balance : {balance}',f'1 matches : {match1}',f'2 matches : {match2}',f'3 matches : {match3}',f'4 matches : {match4}',f'5 matches : {match5}',f'6 matches : {match6}',f'ROI : {roi}'
+
+    return (f'Earnings : {earnings}, Expenses : {expenses}, ROI : {roi}')
 
 print(play_game())
