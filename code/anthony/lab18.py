416f14c7f23631ab6afed3038c5258667f0cab63
Anthony W <anthonyt.wallace@icloud.com>
2019-04-16 13:48:29 -0700

    lab18 update

--- code/anthony/lab18.py
+++ code/anthony/lab18.py
@@ -9,30 +9,11 @@ def main():
     valleys = find_valleys(data)
     peaks_and_valleys = find_peaks_and_valleys(peaks, valleys)
     
-    print_peaks_and_valleys(data)
+    # print_peaks_and_valleys(data)
+    print(print_peaks_and_valleys_with_water(data))
     print(f"Peaks: {peaks}")
     print(f"Valleys: {valleys}")
     print(f"Peaks and Valleys: {peaks_and_valleys}")
-
-
-def print_peaks_and_valleys(data):
-    peaks_and_valleys = ""
-    current_row = max(data)
-    while current_row > 0:
-        for x in data:
-            peaks_and_valleys += '  '
-            if x >= current_row:
-                peaks_and_valleys += 'X'
-            else:
-                peaks_and_valleys += ' '
-        current_row -= 1
-        peaks_and_valleys += '\n'
-    for i in range(len(data)):
-        if i < 10:
-            peaks_and_valleys += '  ' + str(i)
-        else:
-            peaks_and_valleys += ' ' + str(i)
-    print(peaks_and_valleys)
 
 
 def find_peaks(data):
@@ -92,5 +73,67 @@ def find_peaks_and_valleys(peaks, valleys):
     return peaks_and_valleys
 
 
+def print_peaks_and_valleys(data):
+    peaks_and_valleys = ""
+    current_row = max(data)
+    while current_row > 0:
+        for x in data:
+            peaks_and_valleys += '  '
+            if x >= current_row:
+                peaks_and_valleys += 'X'
+            else:
+                peaks_and_valleys += ' '
+        current_row -= 1
+        peaks_and_valleys += '\n'
+    for i in range(len(data)):
+        if i < 10:
+            peaks_and_valleys += '  ' + str(i)
+        else:
+            peaks_and_valleys += ' ' + str(i)
+    print(peaks_and_valleys)
+
+
+def print_peaks_and_valleys_with_water(data):
+    '''
+    
+    '''
+    peaks_and_valleys = ''
+    water_level = 0
+    peak1 = 0
+    peak2 = 0
+    peak3 = 0
+    peaks = [0] + find_peaks(data) + [len(data)-1]
+    current_row = max(data)
+    while current_row > 0:
+        for x in range(len(data)-1):
+            peaks_and_valleys += '  '
+            if x in peaks:
+                peak1 = x
+                if x < len(data) -1:
+                    peak2 = peaks.index(x)
+                    peak3 = peaks[peak2 +1]
+                if data[x] < data[peak3]:
+                    water_level = data[x]
+                else:
+                    water_level = data[peak3]
+            peak1 = data[x]
+            if peak1 >= current_row:
+                peaks_and_valleys += 'X'
+            else:
+                if peak2 < x <= peak3:
+                    if peak1 < water_level >= current_row:
+                        peaks_and_valleys += 'O'
+                    else:
+                        peaks_and_valleys += ' '
+                else:
+                    peaks_and_valleys += ' '
+        peaks_and_valleys += '\n'
+        current_row -= 1
+    for i in range(len(data)):
+        if i < 10:
+            peaks_and_valleys += '  ' + str(i)
+        else:
+            peaks_and_valleys += ' ' + str(i)
+    return peaks_and_valleys
 main()
 
