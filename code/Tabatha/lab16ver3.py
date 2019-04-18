4c694349aa606bf7ecf954477d74a35702ef316a
Tabatha Y <tabathadelane@gmail.com>
2019-04-18 15:37:22 -0700

    lab16v3 w icecream

--- code/Tabatha/lab16ver3.py
+++ code/Tabatha/lab16ver3.py
@@ -3,32 +3,22 @@ from PIL import Image, ImageDraw
 width = 500
 height = 500
 
-img = Image.new('RGB', (width, height))
+img = Image.new('RGB', (width, height), "white")
 
 draw = ImageDraw.Draw(img)
-
-
-# the origin (0, 0) is at the top-left corner
-
-# draw.rectangle(((0, 0), (width, height)), fill="white")
-
-# # draw a rectangle from x0, y0 to x1, y1
-# draw.rectangle(((100, 100), (300, 300)), fill="lightblue")
-
-# # draw a line from x0, y0, x1, y1
-# # using the color pink
-
 
 circle_x = 250
 circle_y = 150
 circle_radius = 40
 draw.ellipse((circle_x-circle_radius, circle_y-circle_radius, circle_x+circle_radius, circle_y+circle_radius), fill='lightgreen')
-
-# color = (256, 128, 128)  # pink
 draw.line((250, 190, 250, 300), fill='lightgreen', width = 4)
 draw.line((190, 225, 310, 225), fill='lightgreen', width = 4)
 draw.line((250, 300, 310, 355), fill='lightgreen', width = 4)
 draw.line((250, 300, 190, 355), fill='lightgreen', width = 4)
-
+circle_x = 310
+circle_y = 205
+circle_radius = 10
+draw.ellipse((circle_x-circle_radius, circle_y-circle_radius, circle_x+circle_radius, circle_y+circle_radius), fill='pink')
+draw.polygon([310, 245, 300, 205, 320, 205], fill ="tan")
 
 img.show()
