e3cbf956a68e74ee81329f336866e4e05b2c193e
Tabatha Y <tabathadelane@gmail.com>
2019-04-18 15:52:03 -0700

    lab16v3 w icecream

--- code/Tabatha/lab16ver3.py
+++ code/Tabatha/lab16ver3.py
@@ -10,15 +10,29 @@ draw = ImageDraw.Draw(img)
 circle_x = 250
 circle_y = 150
 circle_radius = 40
-draw.ellipse((circle_x-circle_radius, circle_y-circle_radius, circle_x+circle_radius, circle_y+circle_radius), fill='lightgreen')
-draw.line((250, 190, 250, 300), fill='lightgreen', width = 4)
-draw.line((190, 225, 310, 225), fill='lightgreen', width = 4)
-draw.line((250, 300, 310, 355), fill='lightgreen', width = 4)
-draw.line((250, 300, 190, 355), fill='lightgreen', width = 4)
+draw.ellipse((circle_x-circle_radius, circle_y-circle_radius, circle_x+circle_radius, circle_y+circle_radius), fill='lightblue')
+draw.line((250, 190, 250, 300), fill='lightblue', width = 4)
+draw.line((190, 225, 310, 225), fill='lightblue', width = 4)
+draw.line((250, 300, 310, 355), fill='lightblue', width = 4)
+draw.line((250, 300, 190, 355), fill='lightblue', width = 4)
+circle_x = 265
+circle_y = 145
+circle_radius = 5
+draw.ellipse((circle_x-circle_radius, circle_y-circle_radius, circle_x+circle_radius, circle_y+circle_radius), fill='blue')
+circle_x = 235
+circle_y = 145
+circle_radius = 5
+draw.ellipse((circle_x-circle_radius, circle_y-circle_radius, circle_x+circle_radius, circle_y+circle_radius), fill='blue')
+draw.arc([235, 165, 265, 180], start = 0, end = 180, fill='blue', width=4)
 circle_x = 310
-circle_y = 205
+circle_y = 187
 circle_radius = 10
 draw.ellipse((circle_x-circle_radius, circle_y-circle_radius, circle_x+circle_radius, circle_y+circle_radius), fill='pink')
+circle_x = 310
+circle_y = 200
+circle_radius = 10
+draw.ellipse((circle_x-circle_radius, circle_y-circle_radius, circle_x+circle_radius, circle_y+circle_radius), fill='lightgreen')
+
 draw.polygon([310, 245, 300, 205, 320, 205], fill ="tan")
 
 img.show()
