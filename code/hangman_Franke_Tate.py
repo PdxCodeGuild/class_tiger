9339a63da19b3fe55511f7da3718ea2ab9f0f3ce
Tate <tateshep@gmail.com>
2019-04-19 12:26:42 -0700

    changed remaining guesses thing

--- code/hangman_Franke_Tate.py
+++ code/hangman_Franke_Tate.py
@@ -23,12 +23,10 @@ words = loads_words(path)
 random_word = 'hello'
 random_word_list = list(random_word)
 
-
-
-
 def check_win(user_word):
     # will run every while loop
     user_word_list = list(user_word)
+
     # sort both user word and random word, see if they are the same
     user_word_list.sort()
     random_word_list.sort()
@@ -39,9 +37,6 @@ def check_win(user_word):
     else:
         print('Keep guessing')
         return False
-
-
-
 
 # Show the user a list of blanks and ask for a letter
 # If the guess letter is in the random word, need to update the users word to show where the match occurs
@@ -58,29 +53,31 @@ def show_user_status(user_word):
     printed = ''.join(print_list)
     return printed
 
-
 def guess_letter(random_word):
     # user has ten guess to guess a given letter
-    count_guesses = 0
+    remaining_guesses = 10
     user_guess = ''
     user_guesses = []
     user_word = []
 
-
-    while count_guesses <= 10:
+    while remaining_guesses != 0:
         user_guess = input('Guess a letter > ').lower()
-        count_guesses += 1
-        if user_guess in random_word_list:
+        if user_guess in user_word:
+            print('You already guessed that')
+            print(f'You have guessed {user_word}')
+        elif user_guess in random_word_list:
             user_word.append(user_guess)
             print(user_word)
             if check_win(user_word) == True:
                 return 'whatever'
-
-
         else:
+            remaining_guesses -= 1
+
             print('incorrect, try again')
         user_guesses.append(user_guess)
         print(show_user_status(user_word))
+    print('\nYou lose')
+    print(f'The word was {random_word}')
     return user_guesses
 
 
