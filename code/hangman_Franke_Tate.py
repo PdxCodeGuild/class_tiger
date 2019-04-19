ff5261d2a7ed6604a1987f9af3f971a00c7dac48
franketokam@gmail.com <franketokam@gmail.com>
2019-04-19 12:12:55 -0700

    2

--- code/hangman_Franke_Tate.py
+++ code/hangman_Franke_Tate.py
@@ -16,7 +16,6 @@ def loads_words(path):
     for item in contents_list:
         if len(item) > 5:
             words.append(item)
-
     return words
 
 words = loads_words(path)
@@ -24,37 +23,12 @@ words = loads_words(path)
 random_word = 'hello'
 random_word_list = list(random_word)
 
-def guess_letter(random_word):
-    # user has ten guess to guess a given letter
-    count_guesses = 0
-    user_guess = ''
-    user_guesses = []
-    user_word = []
 
-    # if the the letter guessed is in the random_word list of letters,
-    # run the update status and show the users updated word with the new matching letter
-    # need to be checking if user word == random word, and if it does, break out of the while loop and return you win
-
-    while count_guesses <= 10:
-        user_guess = input('Guess a letter > ').lower()
-        count_guesses += 1
-        if user_guess in random_word_list:
-            user_word.append(user_guess)
-            print(user_word)
-            if check_win(user_word) == True:
-                return 'whatever'
-
-        else:
-            print('incorrect, try again')
-        user_guesses.append(user_guess)
-
-    return user_guesses
 
 
 def check_win(user_word):
     # will run every while loop
     user_word_list = list(user_word)
-
     # sort both user word and random word, see if they are the same
     user_word_list.sort()
     random_word_list.sort()
@@ -66,23 +40,48 @@ def check_win(user_word):
         print('Keep guessing')
         return False
 
-guess_letter(random_word)
+
 
 
 # Show the user a list of blanks and ask for a letter
 # If the guess letter is in the random word, need to update the users word to show where the match occurs
 
-# def show_user_status(updated_word):
-#     number_letters = len(random_word)
-#
-#     print((' _ ') * number_letters )
-#
-# #If they guess a letter that is in the words variable, show the list of blanks with that letter filled in
-#
-# #
-# show_user_status(user_word)
+def show_user_status(user_word):
+    user_word_list = list(user_word)
+    number_letters = len(random_word)
+    print_list = [' _ ' for x in range(len(random_word_list))]
+
+    for i in range(len(print_list)):
+        if random_word_list[i] in user_word:
+            print_list[i] = random_word_list[i]
+
+    printed = ''.join(print_list)
+    return printed
 
 
+def guess_letter(random_word):
+    # user has ten guess to guess a given letter
+    count_guesses = 0
+    user_guess = ''
+    user_guesses = []
+    user_word = []
 
 
-# print(guess_letter('g'))
+    while count_guesses <= 10:
+        user_guess = input('Guess a letter > ').lower()
+        count_guesses += 1
+        if user_guess in random_word_list:
+            user_word.append(user_guess)
+            print(user_word)
+            if check_win(user_word) == True:
+                return 'whatever'
+
+
+        else:
+            print('incorrect, try again')
+        user_guesses.append(user_guess)
+        print(show_user_status(user_word))
+    return user_guesses
+
+
+guess_letter(random_word)
