1dc2cc54239f4613e2a3a117947f7d2fb227c820
Tate <tateshep@gmail.com>
2019-04-19 14:29:11 -0700

    updated

--- code/hangman_Franke_Tate.py
+++ code/hangman_Franke_Tate.py
@@ -7,10 +7,6 @@ path ='english.txt'
 def loads_words(path):
     with open(path, 'r') as f:
         contents = f.read()
-
-    # split the text file into list strings
-    # return the items in the list that have len > 5
-
     contents_list = contents.split('\n')
     words = []
     for item in contents_list:
@@ -20,26 +16,20 @@ def loads_words(path):
 
 words = loads_words(path)
 random_word = random.choice(words)
-print(random_word)
+print(random_word) ############### TURN THIS ON TO SEE RANDOM WORD
 def check_win(user_word):
-    # will run every while loop
-    # sort both user word and random word, see if they are the same
     a = set(user_word)
     b = set(random_word)
     a = ''.join(sorted(a))
     b = ''.join(sorted(b))
     return a == b
 
-# Show the user a list of blanks and ask for a letter
-# If the guess letter is in the random word, need to update the users word to show where the match occurs
 random_word_list = list(random_word)
-#user_word_list = list(user_word)
 
 def show_user_status(user_word):
     user_word_list = list(user_word)
     number_letters = len(random_word)
     print_list = [' _ ' for x in range(len(random_word_list))]
-
     for i in range(len(print_list)):
         if random_word_list[i] in user_word:
             print_list[i] = random_word_list[i]
@@ -48,7 +38,6 @@ def show_user_status(user_word):
     return printed
 
 def guess_letter(random_word):
-    # user has ten guess to guess a given letter
     remaining_guesses = 10
     user_guess = ''
     user_guesses = []
@@ -62,9 +51,7 @@ def guess_letter(random_word):
             print(f'You have guessed: {user_word}')
         elif user_guess in random_word_list:
             user_word.append(user_guess)
-            print(user_word)
             v = check_win(user_word)
-            # print(v)
             if v:
                 print(f'Good Job! The word was {random_word}')
                 return 'You win!'
@@ -79,6 +66,5 @@ def guess_letter(random_word):
     print('\nYou lose')
     print(f'The word was {random_word}')
     return user_guesses
-
 
 guess_letter(random_word)
