c21f07786aa2ed9a138b80124b207daec9c247d5
franketokam@gmail.com <franketokam@gmail.com>
2019-04-19 14:00:21 -0700

    sort

--- code/hangman_Franke_Tate.py
+++ code/hangman_Franke_Tate.py
@@ -5,7 +5,7 @@ import random
 
 path ='english.txt'
 def loads_words(path):
-    with open(path,'r') as f:
+    with open(path, 'r') as f:
         contents = f.read()
 
     # split the text file into list strings
@@ -20,26 +20,22 @@ def loads_words(path):
 
 words = loads_words(path)
 # random_word = random.choice(words)
-random_word = 'hello'
-random_word_list = list(random_word)
+random_word = list('hello')
+user_word = list('hello')
 
 def check_win(user_word):
     # will run every while loop
-    user_word_list = list(user_word)
-
     # sort both user word and random word, see if they are the same
-    user_word_list.sort()
-    random_word_list.sort()
+    a = ''.join(sorted(user_word))
+    b = ''.join(sorted(random_word))
+    return a == b
 
-    if user_word_list == random_word_list:
-        print('Winner')
-        return True
-    else:
-        print('Keep guessing')
-        return False
+print(check_win(user_word))
 
 # Show the user a list of blanks and ask for a letter
 # If the guess letter is in the random word, need to update the users word to show where the match occurs
+random_word_list = list(random_word)
+#user_word_list = list(user_word)
 
 def show_user_status(user_word):
     user_word_list = list(user_word)
@@ -64,12 +60,16 @@ def guess_letter(random_word):
         user_guess = input('Guess a letter > ').lower()
         if user_guess in user_word:
             print('You already guessed that')
-            print(f'You have guessed {user_word}')
+            print(f'# of guesses remaining: {remaining_guesses}')
+            print(f'already guessed: {user_word}')
         elif user_guess in random_word_list:
             user_word.append(user_guess)
             print(user_word)
-            if check_win(user_word) == True:
-                return 'whatever'
+            v = check_win(user_word)
+            print(v)
+            if v:
+                print('q')
+                return 'You win!'
         else:
             remaining_guesses -= 1
 
