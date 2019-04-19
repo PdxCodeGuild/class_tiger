820c986fcfddf8888883f7a145ba952e69d5d0d9
Mary <mary.com>
2019-04-19 15:38:26 -0700

    updates

--- code/mary/peaks_and_valleys.py
+++ code/mary/peaks_and_valleys.py
@@ -1,8 +1,4 @@
 #peaks_and_valleys.py
-data = [1,2,3,4,5,6,5,4,5,6,7,8,9,8,7,8,9]
-peaks = [data[5], data[12]]
-valleys = [data[7], data [14]]
-peaks_and_valleys = peaks + valleys
-print(data)
-
-#an x for however many data numbers there are
+data =[1,2,3,4,5,6,5,4,5,6,7,8,9,8,7,8,9]
+def peaks(data):
+    
