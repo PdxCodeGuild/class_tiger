b7969f74a3eeb354c751d2e9279f491c37e864fa
Tate <tateshep@gmail.com>
2019-04-25 11:33:15 -0700

    update python-proj

--- code/tate/python-project/main.py
+++ code/tate/python-project/main.py
@@ -1,6 +1,7 @@
 ''' Updates the index.html '''
 
 import requests
+import os
 
 class User:
     def __init__(self,user_color,user_name):
@@ -15,22 +16,74 @@ user_color = input('What is your favorite color? > ').lower()
 
 user = User(user_color,user_name)
 
-print(user)
+def add_comment():
+    os.system('clear')
+    print('Type comments here, or type \'go back\' to go back\n')
+    user_input = input('What would you like to say? > ')
+    if user_input.lower() == 'quit':
+        return False
+    elif user_input.lower() == 'go back':
+        return True
+    user_input = f'\n<h3 style="color:{user.user_color}"> {user.user_name} says: ' + user_input + '</h3>\n'
+
+    with open('templates/my-chatroom.html','a') as f:
+        f.write(user_input)
+
+def login():
+    pass
+
+def create_user():
+    pass
+
+def view_history():
+    pass
+
+def change_color():
+    user_color = input('What would you like to change your color to? > ').lower()
+    return user_color
+
+def input_user_choice():
+    print('What would you like to do? ')
+    print('Choose: comment, change color, view history, quit')
+    user_choice = input(' > ').lower()
+    valid_choices = ['comment','change color','view history','quit']
+    while user_choice not in valid_choices:
+        print('Invalid Input')
+        print('Choose: comment, view history, quit')
+        user_choice = input(' > ').lower()
+
+    return user_choice
 
 def main():
-    # payload = {user.user_name: user.user_color}
-    # r = requests.post('http://0.0.0.0:8000/',data=payload)
+
+    print('Welcome ' + user.user_name)
+    with open('templates/my-chatroom.html','w') as f:
+        f.write('<h1>Welcome <hr></h1>')
 
     while True:
+        os.system('clear')
+        user_choice = input_user_choice()
 
-        user_input = input('What would you like to say? > ')
-        if user_input.lower() == 'quit':
+        if user_choice == 'quit':
             print('Goodbye!')
             break
-        user_input = f'\n<h3 style="color:{user.user_color}"> {user.user_name} says: ' + user_input + '</h3>\n'
-        r = requests.post('http://0.0.0.0:5000/',data=user_input)
-
-        # with open('templates/index.html','a') as f:
-        #     f.write(user_input)
+        elif user_choice == 'comment':
+            add_comment()
+            if add_comment() == False:
+                print('Goodbye!')
+                break
+            elif add_comment() == True:
+                continue
+        elif user_choice == 'change color':
+            user.user_color = change_color()
+        elif user_choice == 'view history':
+            view_history()
 
 main()
+
+
+
+
+# payload = {user.user_name: user.user_color}
+# r = requests.post('http://0.0.0.0:8000/',data=payload)
+# r = requests.post('http://0.0.0.0:5000/old-chatroom.html',data=user_input)
--- code/tate/python-project/runserver.py
+++ code/tate/python-project/runserver.py
@@ -1,11 +1,25 @@
 from flask import Flask, request, render_template
 from flask_socketio import SocketIO
 
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'hellosecrets'
 socketio = SocketIO(app)
 
 @app.route('/')
+def display_index():
+    return render_template('index.html')
+
+@app.route('/my-chatroom')
+def my_chatroom():
+    print("\n\n\nUser in chatroom")
+    # action_counter += 1
+    with open('templates/my-chatroom.html','a') as f:
+        f.write('<p style="position:absolute;top:10px;left:70%;">User actions: ' + str(action_counter) +  '</p>')
+    return render_template('my-chatroom.html')
+
+####### Chatroom Example
+@app.route('/chatroom-example')
 def session():
     return render_template('chatroom.html')
 
@@ -16,12 +30,8 @@ def messageReceived(methods=['GET','POST']):
 def handle_my_custom_event(json,methods =['GET','POST']):
     print('recieved my event: ' + str(json))
     socketio.emit('my response',json,callback=messageReceived)
-
-def just_message(methods=['POST']):
-    print('\n\n\nPOST MESSAGE')
-# Ideas for making it my own.
-    # Allow user to change colors
-    # Add a log page to view the history somehow...
+#######
 
 if __name__ == '__main__':
+    action_counter = 0
     socketio.run(app, debug = True)
