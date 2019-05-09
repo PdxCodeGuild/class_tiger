a13e5cc276acf7a289063ea9e7e273b6c3fc2c4f
Anthony W <anthonyt.wallace@icloud.com>
2019-05-09 16:26:36 -0700

    JavaScript lab 1-3 partB

---
+++ code/anthony/JavaScript/lab1b/lab.js
@@ -0,0 +1,28 @@
+// lab09 as js part B
+
+let input = document.querySelectorAll(".input");
+let output = document.querySelectorAll(".output");
+let bt = document.querySelector('.button');
+let textInput = document.getElementById('input');
+let answer = document.querySelector(".output-text");
+let inUnit;
+let outUnit;
+let solution;
+let value
+
+bt.addEventListener('click', function () {
+    for (let i of input) {
+        if (i.checked == true) {
+            inUnit = parseFloat(i.value);
+        }
+    }
+    for (let j of output) {
+        if (j.checked == true) {
+            outUnit = parseFloat(j.value);
+        }
+    }
+    value = textInput.value
+    solution = ((parseFloat(value) / inUnit) * outUnit);
+    answer.innerText = `Answer: ${solution}`;
+
+})
