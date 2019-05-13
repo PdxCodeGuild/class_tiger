7568decd93530d6e6d5b10f50ce534b3786016ad
franketokam@gmail.com <franketokam@gmail.com>
2019-05-13 14:28:33 -0700

    js 01b

--- code/Franke/Javascript/lab01b/lab01b.js
+++ code/Franke/Javascript/lab01b/lab01b.js
@@ -3,41 +3,30 @@
 // # add it to an output string. Notice that there are 26 letters in the English language,
 // # so encryption is the same as decryption.
 
-// import string as s
-// def rot13():
-//     input_prompt = input(f'input a string')
-//     lower = input_prompt.lower()
-// #lower = [lower[i] for i in range(0, len(lower))]
-// #splitting jammed together word into list of 1 i.e if 3 pass 3 in range
-//     rot1 = 'nopqrstuvwxyzabcdefghijklm'
-//     rot13_list = [rot1[i] for i in range(0, len(rot1))] #adding str to list with list comp
-//     alphabet_list = [s.ascii_lowercase[i] for i in range(0, len(s.ascii_lowercase))]
-//     print(alphabet_list)
-//     rot_dict = {key: value for key, value in zip(alphabet_list, rot13_list)}
-//     output = ''
-//     for i in lower:
-//        if i in rot_dict.keys():
-//             output += rot_dict[i]
-//     return output
-//     print(output)
-////////////
 // for char in text:
 //         index = alphabet.find(char)
 //         output += alphabet_rotated[index]
 //     return output
 
-var input_prompt = prompt('Input a string');
-function rot13(input_prompt) {
-    let input = input_prompt.toLowerCase();
+let input = document.getElementById("input" );
+let button = document.getElementById("button");
+// let output = document.getElementsByTagName("p" );
+let output = document.getElementById("section-b");
+
+function callback(e) {
+    // e.preventDefault();
+    let value = input.value;
+    let lower = value.toLowerCase();
     var rot = 'nopqrstuvwxyzabcdefghijklm';
     var alphabet = 'abcdefghijklmnopqrstuvwyxz';
-    var output = '';
-    for (element of input) {
+    var result = '';
+    for (element of lower) {
        let index = alphabet.indexOf(element);
-       output += rot[index];
+       result += rot[index];
 
     }
-alert(`The rot13 of  ${input_prompt} is ${output}`);
+output.innerText = `The rot13 of  ${value} is ${result}`;
 }
+button.addEventListener('click', callback);
 
-rot13(input_prompt);
+
