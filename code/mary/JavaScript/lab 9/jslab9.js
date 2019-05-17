10775e34fb09cd0ae2942804a35b3dd8e4d97e09
unknown <krzesicki.mary@gmail.com>
2019-05-16 21:52:32 -0700

    lab 9

---
+++ code/mary/JavaScript/lab 9/jslab9.js
@@ -0,0 +1,29 @@
+let req = new XMLHttpRequest();
+let target = document.getElementById("target");
+let submit=document.getElementById("sub");
+let words = document.getElementById("input");
+submit.addEventListener("click", function(){
+    req.addEventListener("progress", function(e) {
+        console.log(e.loaded);
+        target.innerText = "Loading..."
+    });
+    req.addEventListener("error", function(e) {
+        console.log(e.status);
+        target.innerText = "Cannot load quote. Try again later!";
+    });
+    req.addEventListener("load", function(e) {
+        let response = JSON.parse(req.responseText); 
+        for (let i = 0; i < response.quotes.length; i++){
+        let resultHTML = `
+            <p>${response.quotes[i].body}</p>
+            <p><i><a href="${response.quotes[i].url}">${response.quotes[i].author}</a></i></p>
+            `  
+        target.innerHTML = resultHTML;
+        }
+    })
+
+req.open("GET", "https://favqs.com/api/quotes/?filter="+words.value);
+req.setRequestHeader("Authorization", 'Token token=""');
+req.send()
+});
+
