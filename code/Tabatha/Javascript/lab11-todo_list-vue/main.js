e46157751f4ca8418d10bace785e267637c6fd6a
Tabatha Y <tabathadelane@gmail.com>
2019-05-21 15:29:36 -0700

    lab11

--- code/Tabatha/Javascript/lab11-todo_list-vue/main.js
+++ code/Tabatha/Javascript/lab11-todo_list-vue/main.js
@@ -1,32 +1,47 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        message: 'What do you need to do?',
+        newtodo: '',
+        todos: [
+            {'text': 'Dishes', 'checkStyle': "checkOn", "done": true, "undo": false},
+            {'text': 'Laundry', 'checkStyle': "checkOn", "done": true, "undo": false}
+        ],
+        done: [],
+    },
+    methods: {
+        addTodo: function() {
+            this.todos.push({'text': `${this.newtodo}`, 'checkStyle': "checkOn", "done": true, "undo": false})
+            console.log(this.newtodo)
+        },
+        finishTodo: function(todo) {
+            console.log(todo)
+            index = this.todos.indexOf(todo);
+            console.log(index)
+            this.done.push(todo);
+            // todo.checkStyle = "checkOff";
+            todo.done = false;
+            todo.undo = true;
+            this.todos.splice(index, 1);
+        },
+        deleteTodo: function(todo) {
+            index = this.todos.indexOf(todo);
+            this.todos.splice(index, 1);
+        },
+        deleteDone: function(d) {
+            index = this.done.indexOf(d);
+            this.done.splice(index, 1);
+        },
+        undoDone(d){
+            index = this.done.indexOf(d);
+            console.log(index)
+            this.todos.push(d);
+            // d.checkStyle = "checkOn";
+            d.done = true;
+            d.undo = false;
+            this.done.splice(index, 1);
+        }
+    },
+});
 
-
-var taskList = document.getElementById("list");
-var doneList = document.getElementById("complete");
-
-var trash = document.getElementsByClassName("trash");
-
-function addTask(){
-    var task = document.createElement("li");
-    var newTask = document.getElementById("newtask").value;
-    var t = document.createTextNode(newTask);
-    var complete = document.createElement("button");
-    complete.className = "check";
-    complete.innerHTML ="<i class='fas fa-clipboard-check'></i>";
-    complete.addEventListener("click", function(){
-        console.log("clicked")
-        doneList.appendChild(this.parentElement);
-        this.remove();
-    })
-    task.appendChild(complete);
-    document.getElementById("newtask").value = "";
-    var trashBtn = document.createElement("button");
-    var icon = document.createElement("i");
-    trashBtn.className = "trashBtn";
-    trashBtn.innerHTML = "<i class='fas fa-trash-alt'></i>";
-    trashBtn.addEventListener("click", function(){
-        this.parentElement.remove();
-    })
-    task.appendChild(trashBtn);
-    taskList.appendChild(task);
-    task.appendChild(t);
-}
+// make on delete function by making one array and using a filter
