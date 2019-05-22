5b15d738951b8aa64f8c128b01cb088cf66de11f
Tate <tateshep@gmail.com>
2019-05-21 19:48:47 -0700

    lab10

--- code/tate/Labs/JavaScript/lab10_any-API/app.js
+++ code/tate/Labs/JavaScript/lab10_any-API/app.js
@@ -8,35 +8,79 @@ var responseSection = document.querySelector('#response-section');
 var responseSection2 = document.querySelector('#response-section2');
 var responseSection3 = document.querySelector('#response-section3');
 var goBtn = document.getElementById('go-btn');
-
+var myInput = document.getElementById('my-input').value;
+var inputObj = document.getElementById('my-input');
+var galleryH1 = document.getElementById('gallery-h1');
+var lightbox = document.getElementById('lightbox');
+var lightboxImg = document.getElementById('lightbox-img');
+var closeBtn = document.getElementById('close-btn');
 var flickr = new Flickr(token);
 var htmlContent;
 
-function photoSearch(){
+function closeBtnFunc(){
+  lightbox.style.top = '-1000px';
+}
+
+function dispLightbox() {
+  imgSrc = this.getAttribute("src");
+  lightboxImg.setAttribute("src",imgSrc);
+  lightbox.style.top = '0px';
+  console.log(imgSrc);
+}
+
+function random15Func () {
+  let random15 = [];
+
+  for(let i = 0; i < 15; i++) {
+
+    myRandom = Math.floor(Math.random()*100)
+    while (random15.includes(myRandom)){
+      myRandom = Math.floor(Math.random()*100)
+    }
+    random15.push(myRandom);
+  }
+  console.log(random15);
+  return random15;
+}
+
+function goBtnFunc () {
+  myInput = document.getElementById('my-input').value;
+  galleryH1.innerText = myInput.toUpperCase();
+  photoSearch(myInput);
+}
+
+function photoSearch(myInput){
+
+  random15 = random15Func();
+  let responseSectionArray = [responseSection,responseSection2,responseSection3];
+  responseSectionArray.forEach(function(element){
+    element.innerHTML = "";
+  })
+
   flickr.photos.search({
-    text: 'mountains'
+    text: myInput
 
   }).then(function (res) {
     console.log('yay!', res.body);
     let myphotos = res.body.photos.photo;
 
-    function createImgElem(i,section) {
-      let photo = res.body.photos.photo[i];
-
+    for (let i=0; i < 5 ; i++){
+      createImgElem(random15[i],0);
+    }
+    for (let i=6; i < 11 ; i++){
+      createImgElem(random15[i],1);
+    }
+    for (let i=11; i < 16 ; i++){
+      createImgElem(random15[i],2);
+    }
+    function createImgElem(input,num) {
+      let photo = myphotos[input];
       let imgElem = document.createElement('img');
       imgElem.setAttribute('src',`https://farm${photo.farm}.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}.jpg`);
       imgElem.classList.add('my-img');
-      responseSection.appendChild(imgElem);
-    }
-
-    for (let i=0; i < 5 ; i++){
-      createImgElem(i);
-    }
-    for (let i=6; i < 11 ; i++){
-      createImgElem(i);
-    }
-    for (let i=11; i < 16 ; i++){
-      createImgElem(i);
+      imgElem.classList.add(`response-section${num}`);
+      imgElem.addEventListener('click',dispLightbox);
+      responseSectionArray[num].appendChild(imgElem);
     }
 
   }).catch(function (err) {
@@ -44,4 +88,13 @@ function photoSearch(){
   });
 }
 
-goBtn.addEventListener('click',photoSearch);
+photoSearch('landscape');
+
+inputObj.addEventListener("keydown", function(event){
+  if(event.keyCode === 13){
+    event.preventDefault();
+    goBtn.click();
+  }
+});
+goBtn.addEventListener('click',goBtnFunc);
+closeBtn.addEventListener('click',closeBtnFunc);
