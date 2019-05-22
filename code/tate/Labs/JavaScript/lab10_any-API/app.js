7df5dabea04b0d2adfa01f78c119bb1e369af715
Tate <tateshep@gmail.com>
2019-05-22 15:51:08 -0700

    lab10 update

--- code/tate/Labs/JavaScript/lab10_any-API/app.js
+++ code/tate/Labs/JavaScript/lab10_any-API/app.js
@@ -13,33 +13,61 @@ var inputObj = document.getElementById('my-input');
 var galleryH1 = document.getElementById('gallery-h1');
 var lightbox = document.getElementById('lightbox');
 var lightboxImg = document.getElementById('lightbox-img');
+var photoDescription = document.getElementById('photo-description');
+var photoTitle = document.getElementById('photo-title');
+var photoOwner = document.getElementById('photo-owner');
+var photoTags = document.getElementById('photo-tags');
+var photoViews = document.getElementById('photo-views');
+
 var closeBtn = document.getElementById('close-btn');
 var flickr = new Flickr(token);
 var htmlContent;
 
-function closeBtnFunc(){
-  lightbox.style.top = '-1000px';
-}
-
 function dispLightbox() {
+  function getInfo () {
+    flickr.photos.getInfo({
+      photo_id: photoId
+    }).then(function (res) {
+      console.log('got photo info!', res.body);
+      let lightboxPhoto = res.body.photo;
+      let tags = lightboxPhoto.tags.tag;
+
+      photoTitle.innerText = lightboxPhoto.title['_content'];
+      photoDescription.innerText = lightboxPhoto.description['_content'];
+      photoOwner.innerText = lightboxPhoto.owner['username'];
+      photoTags.innerText = '';
+      photoViews.innerText = lightboxPhoto.views;
+      for (let i = 0; i < tags.length ; i ++){
+        photoTags.innerHTML += tags[i]._content + ', ';
+      }
+
+    }).catch(function (err) {
+      console.error('bonk', err);
+    });
+  }
+
+  let photoId = this.getAttribute('id')
   imgSrc = this.getAttribute("src");
   lightboxImg.setAttribute("src",imgSrc);
   lightbox.style.top = '0px';
-  console.log(imgSrc);
+  console.log(this.getAttribute('id'));
+  getInfo();
+}
+function closeBtnFunc(){
+  lightbox.style.top = '-1200px';
 }
 
-function random15Func () {
+function random15Func (length) {
   let random15 = [];
 
   for(let i = 0; i < 15; i++) {
 
-    myRandom = Math.floor(Math.random()*100)
+    myRandom = Math.floor(Math.random()*length)
     while (random15.includes(myRandom)){
-      myRandom = Math.floor(Math.random()*100)
+      myRandom = Math.floor(Math.random()*length)
     }
     random15.push(myRandom);
   }
-  console.log(random15);
   return random15;
 }
 
@@ -51,7 +79,6 @@ function goBtnFunc () {
 
 function photoSearch(myInput){
 
-  random15 = random15Func();
   let responseSectionArray = [responseSection,responseSection2,responseSection3];
   responseSectionArray.forEach(function(element){
     element.innerHTML = "";
@@ -61,24 +88,26 @@ function photoSearch(myInput){
     text: myInput
 
   }).then(function (res) {
+
     console.log('yay!', res.body);
     let myphotos = res.body.photos.photo;
+    let random15 = random15Func(myphotos.length);
 
     for (let i=0; i < 5 ; i++){
-      createImgElem(random15[i],0);
+      createImgElem( random15[i],0);
     }
-    for (let i=6; i < 11 ; i++){
-      createImgElem(random15[i],1);
+    for (let i=5; i < 10 ; i++){
+      createImgElem( random15[i],1);
     }
-    for (let i=11; i < 16 ; i++){
-      createImgElem(random15[i],2);
+    for (let i=10; i < 15 ; i++){
+      createImgElem( random15[i],2);
     }
     function createImgElem(input,num) {
-      let photo = myphotos[input];
+      photo = myphotos[input];
       let imgElem = document.createElement('img');
       imgElem.setAttribute('src',`https://farm${photo.farm}.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}.jpg`);
       imgElem.classList.add('my-img');
-      imgElem.classList.add(`response-section${num}`);
+      imgElem.setAttribute('id',`${photo.id}`);
       imgElem.addEventListener('click',dispLightbox);
       responseSectionArray[num].appendChild(imgElem);
     }
@@ -88,7 +117,7 @@ function photoSearch(myInput){
   });
 }
 
-photoSearch('landscape');
+photoSearch('Tate Shepherd Photography');
 
 inputObj.addEventListener("keydown", function(event){
   if(event.keyCode === 13){
