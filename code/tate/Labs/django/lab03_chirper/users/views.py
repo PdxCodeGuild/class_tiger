71df040934848a3330dddf0619f5dd45eaf00fb2
Tate <tateshep@gmail.com>
2019-06-07 10:41:35 -0700

    update

--- code/tate/Labs/django/lab03_chirper/users/views.py
+++ code/tate/Labs/django/lab03_chirper/users/views.py
@@ -10,13 +10,6 @@ class UserListView(generic.ListView):
     model = User
     template_name = 'user_list.html'
 
-    # def get_num_chirps(self):
-    #     chirp_objects = Chirp.objects.filter(author__username__exact=self.kwargs['username'])
-    #     num_chirps = len(chirp_objects)
-    #     print(num_chirps)
-    #     return num_chirps
-
-
 class SignUpView(generic.CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('login')
@@ -28,6 +21,4 @@ class ChirpList(generic.ListView):
 
     def get_queryset(self):
         chirp_objects = Chirp.objects.filter(author__username__exact=self.kwargs['username']).order_by('-created')
-        # num_chirps = len(chirp_objects)
-        # print(num_chirps)
         return chirp_objects
